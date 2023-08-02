0cdd8096bf2c2d934bbd17b8055abd377ce42399
Капибара <fatima.atakishieva@yandex.ru>
2023-08-02 13:32:46 +0300

    дз занятие 7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+    #Заголовок
+    #Небольшое текстовое поле
+    title = models.CharField("Заголовок", max_length=128)
+
+    # Описание
+    description = models.TextField("Описание")
+
+    # Цена
+    # Число с фиксированной точностью
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+
+    # Дата создания
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Дата изменения
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Уместен ли торг
+    # Логическое значение
+    auction = models.BooleanField("Торг", help_text="Отметьте, если хотите торговться")
+
+    def __str__(self):
+        return f"<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>"
+
+    class Meta:
+        db_table = 'advertisements'
+
+    # Изображение
+
+    # Адрес
+
+    # Отзывы/рейтинг продавца
+
+    # Контакты продавца
+
+    # Похожие товары
